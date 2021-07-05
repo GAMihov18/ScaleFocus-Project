2558a033497d2c82b3ea76710527b2ef86b00bd4
GAMihov18@codingburgas.bg <GAMihov18@codingburgas.bg>
2021-07-05 13:11:01 +0300

    Add C# project

---
+++ Project App/Project App/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Manager;
+using Manager.Users;
+using Manager.Teams;
+using Manager.Tasks;
+
+
+namespace Project_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
