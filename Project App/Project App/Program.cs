7913697a0d002dfc33ed83fb786aa060a8ef1634
GAMihov18@codingburgas.bg <GAMihov18@codingburgas.bg>
2021-07-07 17:36:19 +0300

    Finish User Management View structure

--- Project App/Project App/Program.cs
+++ Project App/Project App/Program.cs
@@ -21,8 +21,9 @@ namespace Project_App
 			string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ProjectDB;Integrated Security=True";
 			SqlConnection connection = new SqlConnection(connectionString);
 			connection.Open();
-			LoginSystem printer = new LoginSystem();
-			printer.MainMenu(printer.PrintLogin(connection));
+			LoginSystem printer = new LoginSystem(connection);
+			printer.MainMenu(printer.PrintLogin());
+			
 			connection.Close();
 		}
 	}
