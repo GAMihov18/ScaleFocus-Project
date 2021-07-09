c7d5a76b84135661edd352a9a9a0fee2fe545284
GAMihov18@codingburgas.bg <GAMihov18@codingburgas.bg>
2021-07-09 15:06:23 +0300

    Start work on teams

--- Project App/Project App/Team.cs
+++ Project App/Project App/Team.cs
@@ -12,16 +12,24 @@ namespace Manager
         class Team
         {
 
+            SqlConnection connection;
+            public Team(SqlConnection connection)
+            {
+                this.connection = connection;
+            }
+            public int Id { get { return id; } set { id = value; } }
+            public string Title { get { return title; } set { title = value; } }
+            public DateTime CreationDate { get { return creationDate; } set { creationDate = value; } }
+            public int CreatorId { get { return creatorId; } set { creatorId = value; } }
+            public DateTime LastChangeDate { get { return lastChangeDate; } set { lastChangeDate = value; } }
+            public int LastChangeUserId { get { return lastChangeUserId; } }
+
             private int id;
             private string title;
             private DateTime creationDate;
             private int creatorId;
             private DateTime lastChangeDate;
             private int lastChangeUserId;
-
-            public int Id { get { return id; } set { id = value; } }
-            public string Title { get { return title; }set { title = value; } }
-
 
         }
     }
